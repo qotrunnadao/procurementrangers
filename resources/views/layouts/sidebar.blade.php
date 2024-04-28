d00f9e0de2004e8cf9d0c8bba781fea74ab771d9
Qotrunnada Oktiriani <qotrunnadaoktiriani@gmail.com>
2024-04-28 11:25:14 +0700

    fixed sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,13 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
         <div class="sidebar-brand-text mx-3">PROCUREMENT</div>
     </a>
     <hr class="sidebar-divider my-0">
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('home') }}"">
+    <li class="nav-item {{ Request::is('home*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('home') }}">
             <i class=" fa fa-home"></i>
             <span>Home</span></a>
     </li>
